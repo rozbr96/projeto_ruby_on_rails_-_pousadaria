85f542a0ce97427f0d0ff34bb55757df909ce718
Rosemilson <rozbr96@gmail.com>
2023-11-04 21:24:28 -0300

    Implement basic own inn room creation

--- app/controllers/own_inn_rooms_controller.rb
+++ app/controllers/own_inn_rooms_controller.rb
@@ -1,6 +1,36 @@
 
 class OwnInnRoomsController < ApplicationController
+  def create
+    @room = InnRoom.new room_params
+    @room.inn = current_innkeeper.inn
+
+    if @room.save
+      redirect_to own_inn_room_path(@room), notice: 'Quarto registrado com sucesso'
+    else
+      flash.now[:alert] = 'Erro ao registrar quarto'
+      render :new
+    end
+
+  end
+
   def index
     @rooms = current_innkeeper.inn.inn_rooms
+  end
+
+  def new
+    @room = InnRoom.new
+  end
+
+  def show
+    @room = InnRoom.find params[:id]
+  end
+
+  private
+
+  def room_params
+    params.require(:inn_room).permit :name, :description, :maximum_number_of_guests,
+      :number_of_bathrooms, :number_of_wardrobes, :has_balcony, :has_air_conditioning,
+      :has_tv, :has_vault, :is_accessible_for_people_with_disabilities, :enabled,
+      :dimension, :price
   end
 end
---
+++ spec/system/own_inn_rooms/create_room_spec.rb
@@ -0,0 +1,117 @@
+
+require 'rails_helper'
+
+describe 'User visits room creation page' do
+  context 'when logged in as innkeeper' do
+    before :all do
+      @innkeeper = Innkeeper.create! name: 'Gui', email: 'gui@test.com', password: 'password'
+
+      @inn = Inn.create! name: 'Pousada Universal', corporate_name: 'Pousada Universal LTDA',
+        registration_number: '11338082000103', description: 'Pousada universal...',
+        pets_are_allowed: true, usage_policies: 'Está estritamente proibido...',
+        email: 'pousada.universal@test.com', enabled: false, innkeeper: @innkeeper,
+        check_in: '10:00', check_out: '10:00'
+
+      Address.create! street: 'Rua Galática', number: '42',
+        neighbourhood: 'Virgem', city: 'Terra', state: 'Via Láctea',
+        postal_code: '01.137-000', complement: 'Shaka', inn: @inn
+    end
+
+    after :all do
+      Address.delete_all
+      Inn.delete_all
+      Innkeeper.delete_all
+    end
+
+    it 'from the home page' do
+      login_as @innkeeper
+
+      visit root_path
+
+      within 'nav' do
+        click_on @innkeeper.name
+        click_on 'Minha Pousada'
+      end
+
+      click_on 'Visualizar Quartos'
+      click_on 'Registrar Quarto'
+
+      expect(current_path).to eq new_own_inn_room_path
+    end
+
+    it 'and sees the registration form' do
+      login_as @innkeeper
+
+      visit new_own_inn_room_path
+
+      within 'form#inn-room-form' do
+        expect(page).to have_field 'Nome'
+        expect(page).to have_field 'Descrição'
+        expect(page).to have_field 'Dimensão'
+        expect(page).to have_field 'Diária'
+        expect(page).to have_field 'Número Máximo de Convidados'
+        expect(page).to have_field 'Número de Banheiros'
+        expect(page).to have_field 'Número de Guarda-roupas'
+        expect(page).to have_field 'Possui sacada?'
+        expect(page).to have_field 'Possui TV?'
+        expect(page).to have_field 'Possui Ar Condicionado?'
+        expect(page).to have_field 'Possui Cofre?'
+        expect(page).to have_field 'É acessível para pessoas com deficiência?'
+        expect(page).to have_field 'Habilitado?'
+      end
+    end
+
+    it 'and creates the room successfully' do
+      login_as @innkeeper
+
+      visit new_own_inn_room_path
+
+      within 'form#inn-room-form' do
+        fill_in 'Nome', with: 'Sol'
+        fill_in 'Descrição', with: 'Quarto arejado, com vista para o mar...'
+        fill_in 'Dimensão', with: '400'
+        fill_in 'Diária', with: '40000'
+        fill_in 'Número Máximo de Convidados', with: '4'
+        fill_in 'Número de Banheiros', with: '2'
+        fill_in 'Número de Guarda-roupas', with: '2'
+        check 'Possui sacada?'
+        check 'Possui TV?'
+        uncheck 'Possui Ar Condicionado?'
+        uncheck 'Possui Cofre?'
+        uncheck 'É acessível para pessoas com deficiência?'
+        check 'Habilitado?'
+        click_on 'Criar Quarto de Pousada'
+      end
+
+      expect(current_path).to eq own_inn_room_path InnRoom.last
+      expect(page).to have_content 'Quarto registrado com sucesso'
+    end
+
+    it 'and fails to create the room, seeing the related errors' do
+      login_as @innkeeper
+
+      visit new_own_inn_room_path
+
+      within 'form#inn-room-form' do
+        fill_in 'Nome', with: ''
+        fill_in 'Descrição', with: 'Quarto arejado, com vista para o mar...'
+        fill_in 'Dimensão', with: ''
+        fill_in 'Diária', with: '40000'
+        fill_in 'Número Máximo de Convidados', with: '4'
+        fill_in 'Número de Banheiros', with: '2'
+        fill_in 'Número de Guarda-roupas', with: '2'
+        check 'Possui sacada?'
+        check 'Possui TV?'
+        uncheck 'Possui Ar Condicionado?'
+        uncheck 'Possui Cofre?'
+        uncheck 'É acessível para pessoas com deficiência?'
+        check 'Habilitado?'
+        click_on 'Criar Quarto de Pousada'
+      end
+
+      expect(page).to have_content 'Erro ao registrar quarto'
+      expect(page).to have_content 'Nome não pode ficar em branco'
+      expect(page).to have_content 'Dimensão não pode ficar em branco'
+    end
+  end
+end
