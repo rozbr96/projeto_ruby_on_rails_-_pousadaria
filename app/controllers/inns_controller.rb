84bc631f21eaefece1e5402c197bef686cd7fe1a
Rosemilson <rozbr96@gmail.com>
2023-11-07 22:15:45 -0300

    Implement inns listing

--- app/controllers/inns_controller.rb
+++ app/controllers/inns_controller.rb
@@ -1,7 +1,10 @@
 
 class InnsController < ApplicationController
   def index
-    @inns = Inn.where enabled: true
+    inns = Inn.where(enabled: true).order(created_at: :desc).to_a
+
+    @newest_inns = inns.shift 3
+    @remaining_inns = inns
   end
 
   def show
--- spec/system/inns/list_inns_spec.rb
+++ spec/system/inns/list_inns_spec.rb
@@ -15,33 +15,6 @@ describe 'User visits inns listing page' do
   end
 
   context 'with existing inns' do
-    it 'and sees existing inns' do
-      innkeeper = Innkeeper.create! name: 'Gui', email: 'gui@test.com', password: 'password'
-      inn = Inn.create!(
-        name: 'Pousada Universal', corporate_name: 'Pousada Universal LTDA',
-        registration_number: '11338082000103', description: 'Pousada universal...',
-        pets_are_allowed: true, usage_policies: 'Está estritamente proibido...',
-        email: 'pousada.universal@test.com', enabled: true, innkeeper: innkeeper,
-        check_in: '10:00', check_out: '10:00'
-      )
-
-      Address.create!(
-        street: 'Rua Galática', number: '42', neighbourhood: 'Virgem',
-        city: 'Terra', state: 'Via Láctea', postal_code: '01.137-000',
-        complement: 'Shaka', inn: inn
-      )
-
-      visit root_path
-
-      within 'nav' do
-        click_on 'Pousadas'
-      end
-
-      expect(current_path).to eq inns_path
-      expect(page).to have_content inn.name
-      expect(page).to have_content inn.address.city
-    end
-
     it 'and sees no available inns' do
       innkeeper = Innkeeper.create! name: 'Gui', email: 'gui@test.com', password: 'password'
       inn = Inn.create!(
@@ -59,6 +32,33 @@ describe 'User visits inns listing page' do
       end
 
       expect(page).to have_content 'Nenhuma pousada disponível'
+    end
+
+    it 'and sees available inns' do
+      10.times do
+        innkeeper = FactoryBot.create :innkeeper
+        inn = FactoryBot.create :inn, innkeeper: innkeeper, enabled: true
+        FactoryBot.create :address, inn: inn
+      end
+
+      inns = Inn.order(created_at: :desc).to_a
+      newest_inns = inns.shift 3
+
+      visit root_path
+
+      within '#newest-inns' do
+        newest_inns.each do |inn|
+          expect(page).to have_content inn.name
+          expect(page).to have_content inn.address.city
+        end
+      end
+
+      within '#inns' do
+        inns.each do |inn|
+          expect(page).to have_content inn.name
+          expect(page).to have_content inn.address.city
+        end
+      end
     end
   end
 end
