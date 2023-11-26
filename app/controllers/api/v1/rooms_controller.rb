5ccd658a10fb13d990698df1b9e66237d79baa96
Rosemilson <rozbr96@gmail.com>
2023-11-26 06:20:44 -0300

    Change rooms indexing from using select to where

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -34,7 +34,7 @@ class Api::V1::RoomsController < Api::V1::BasicController
 
   def index
     inn = Inn.find params[:inn_id]
-    rooms = inn.inn_rooms.select &:enabled?
+    rooms = inn.inn_rooms.where enabled: true
 
     render status: :ok, json: rooms
   end
