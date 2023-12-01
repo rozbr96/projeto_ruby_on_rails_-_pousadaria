c86d6be75ca6969f2332544a5cc789626ecf1913
Rosemilson <rozbr96@gmail.com>
2023-12-01 07:08:38 -0300

    Create some initial data within the seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,34 @@ payment_methods = 'Dinheiro', 'Boleto', 'PIX', 'Cartão de Crédito',
 payment_methods.each do |payment_method|
   PaymentMethod.create name: payment_method, enabled: true
 end
+
+
+payment_methods_ids = PaymentMethod.pluck :id
+
+
+innkeeper = FactoryBot.create :innkeeper, email: 'innkeeper@yandex.com', password: 'password'
+inn = FactoryBot.create :inn, innkeeper: innkeeper
+FactoryBot.create :address, inn: inn
+FactoryBot.create :guest, email: 'guest@yandex.com', password: 'password'
+
+payment_methods_ids.shuffle.take(3).each do |payment_method_id|
+  InnPaymentMethod.create! inn: inn, payment_method_id: payment_method_id
+end
+
+4.times do
+  FactoryBot.create :inn_room, inn: inn
+end
+
+100.times do
+  innkeeper = FactoryBot.create :innkeeper, password: 'password'
+  inn = FactoryBot.create :inn, innkeeper: innkeeper
+  FactoryBot.create :address, inn: inn
+
+  payment_methods_ids.shuffle.take(3).each do |payment_method_id|
+    InnPaymentMethod.create! inn: inn, payment_method_id: payment_method_id
+  end
+
+  4.times do
+    FactoryBot.create :inn_room, inn: inn
+  end
+end
